1c03d13789f68de4693f5e0159e72de9f19a2316
wisehead <alex.chenhui@gmail.com>
2019-03-22 16:41:43 +0800

    4.variable

---
+++ 4.variable/variable1.lua
@@ -0,0 +1,18 @@
+a = 5               -- 全局变量
+local b = 5         -- 局部变量
+
+function joke()
+    c = 5           -- 全局变量
+    local d = 6     -- 局部变量
+end
+
+joke()
+print(c,d)          --> 5 nil
+
+do 
+    local a = 6     -- 局部变量
+    b = 6           -- 对局部变量重新赋值
+    print(a,b);     --> 6 6
+end
+
+print(a,b)      --> 5 6
