c95e76925a97e4b3c1cb5c035050bfc7ba9781e6
wisehead <alex.chenhui@gmail.com>
2019-03-22 16:13:30 +0800

    bool and nubmer

---
+++ 3.datatype/boolean.lua
@@ -0,0 +1,9 @@
+print(type(true))
+print(type(false))
+print(type(nil))
+ 
+if false or nil then
+    print("至少有一个是 true")
+else
+    print("false 和 nil 都为 false!")
+end
---
+++ 3.datatype/number.lua
@@ -0,0 +1,6 @@
+print(type(2))
+print(type(2.2))
+print(type(0.2))
+print(type(2e+1))
+print(type(0.2e-1))
+print(type(7.8263692594256e-06))
