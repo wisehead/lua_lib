60ef576aa29f68a940af52c7221fb764286eb6ea
wisehead <alex.chenhui@gmail.com>
2019-03-22 16:34:44 +0800

    funtion

---
+++ 3.datatype/function2.lua
@@ -0,0 +1,13 @@
+function testFun(tab,fun)
+    for k ,v in pairs(tab) do
+        print(fun(k,v));
+    end
+end
+
+
+tab={key1="val1",key2="val2"};
+testFun(tab,
+function(key,val)--匿名函数
+    return key.."="..val;
+end
+);
