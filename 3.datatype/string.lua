2ba3d0dea31c6249637d819eca0154f6b73e1065
wisehead <alex.chenhui@gmail.com>
2019-03-22 16:25:27 +0800

    string.lua

---
+++ 3.datatype/string.lua
@@ -0,0 +1,22 @@
+html = [[
+<html>
+<head></head>
+<body>
+    <a href="http://www.runoob.com/">菜鸟教程</a>
+</body>
+</html>
+]]
+print(html)
+
+string1 = "this is string1"
+string2 = 'this is string2'
+print(string1)
+print(string2)
+print("2" + 6)
+print("2" + "6")
+print("2 + 6")
+print("a" .. 'b')
+print(157 .. 428)
+len = "www.runoob.com"
+print(#len)
+print(#"www.runoob.com")
