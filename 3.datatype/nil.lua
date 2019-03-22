81b8630ac4613cd009d638a6980a59b0b9e8bbb2
wisehead <alex.chenhui@gmail.com>
2019-03-22 16:11:18 +0800

    3.datatype

---
+++ 3.datatype/nil.lua
@@ -0,0 +1,9 @@
+tab1 = { key1 = "val1", key2 = "val2", "val3" }
+for k, v in pairs(tab1) do
+    print(k .. " - " .. v)
+end
+ 
+tab1.key1 = nil
+for k, v in pairs(tab1) do
+    print(k .. " - " .. v)
+end
