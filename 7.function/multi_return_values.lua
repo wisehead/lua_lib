143f9751d19a7c8005910bf85e5124a015a152ee
wisehead <alex.chenhui@gmail.com>
2019-03-25 13:47:49 +0800

    funtion part2

---
+++ 7.function/multi_return_values.lua
@@ -0,0 +1,13 @@
+function maximum (a)
+    local mi = 1             -- 最大值索引
+    local m = a[mi]          -- 最大值
+    for i,val in ipairs(a) do
+       if val > m then
+           mi = i
+           m = val
+       end
+    end
+    return m, mi
+end
+
+print(maximum({8,10,23,12,5}))
---
+++ 7.function/varargs.lua
@@ -0,0 +1,8 @@
+function add(...)  
+local s = 0  
+  for i, v in ipairs{...} do   --> {...} 表示一个由所有变长参数构成的数组  
+    s = s + v  
+  end  
+  return s  
+end  
+print(add(3,4,5,6,7))
