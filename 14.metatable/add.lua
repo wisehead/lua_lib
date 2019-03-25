22d10d7655f08a2242e3368a991c79bc20b38b38
wisehead <alex.chenhui@gmail.com>
2019-03-25 18:16:34 +0800

    14.metatable part2

---
+++ 14.metatable/add.lua
@@ -0,0 +1,28 @@
+-- 计算表中最大值，table.maxn在Lua5.2以上版本中已无法使用
+-- 自定义计算表中最大键值函数 table_maxn，即计算表的元素个数
+function table_maxn(t)
+    local mn = 0
+    for k, v in pairs(t) do
+        if mn < k then
+            mn = k
+        end
+    end
+    return mn
+end
+
+-- 两表相加操作
+mytable = setmetatable({ 1, 2, 3 }, {
+  __add = function(mytable, newtable)
+    for i = 1, table_maxn(newtable) do
+      table.insert(mytable, table_maxn(mytable)+1,newtable[i])
+    end
+    return mytable
+  end
+})
+
+secondtable = {4,5,6}
+
+mytable = mytable + secondtable
+    for k,v in ipairs(mytable) do
+print(k,v)
+end
---
+++ 14.metatable/tostring.lua
@@ -0,0 +1,10 @@
+mytable = setmetatable({ 10, 20, 30 }, {
+  __tostring = function(mytable)
+    sum = 0
+    for k, v in pairs(mytable) do
+        sum = sum + v
+    end
+    return "表所有元素的和为 " .. sum
+  end
+})
+print(mytable)
