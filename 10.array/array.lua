63bde963b4f44ad8ceaf7268679fac83d1e718b1
wisehead <alex.chenhui@gmail.com>
2019-03-25 15:08:36 +0800

    10.array

---
+++ 10.array/array.lua
@@ -0,0 +1,5 @@
+array = {"Lua", "Tutorial"}
+
+for i= 0, 2 do
+   print(array[i])
+end
---
+++ 10.array/multi_dimension2.lua
@@ -0,0 +1,16 @@
+-- 初始化数组
+array = {}
+maxRows = 3
+maxColumns = 3
+for row=1,maxRows do
+   for col=1,maxColumns do
+      array[row*maxColumns +col] = row*col
+   end
+end
+
+-- 访问数组
+for row=1,maxRows do
+   for col=1,maxColumns do
+      print(array[row*maxColumns +col])
+   end
+end
