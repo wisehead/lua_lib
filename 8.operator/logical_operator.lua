e2cce55bcc47314319791afa0cb3b0ce5b6f8915
wisehead <alex.chenhui@gmail.com>
2019-03-25 14:03:47 +0800

    8.operator part2

---
+++ 8.operator/logical_operator.lua
@@ -0,0 +1,32 @@
+a = true
+b = true
+
+if ( a and b )
+then
+   print("a and b - 条件为 true" )
+end
+
+if ( a or b )
+then
+   print("a or b - 条件为 true" )
+end
+
+print("---------分割线---------" )
+
+-- 修改 a 和 b 的值
+a = false
+b = true
+
+if ( a and b )
+then
+   print("a and b - 条件为 true" )
+else
+   print("a and b - 条件为 false" )
+end
+
+if ( not( a and b) )
+then
+   print("not( a and b) - 条件为 true" )
+else
+   print("not( a and b) - 条件为 false" )
+end
---
+++ 8.operator/special_operator.lua
@@ -0,0 +1,10 @@
+a = "Hello "
+b = "World"
+
+print("连接字符串 a 和 b ", a..b )
+
+print("b 字符串长度 ",#b )
+
+print("字符串 Test 长度 ",#"Test" )
+
+print("菜鸟教程网址长度 ",#"www.runoob.com" )
