e834f1a2a174d5650adf7b3dd595802b2095087a
wisehead <alex.chenhui@gmail.com>
2019-03-25 16:25:34 +0800

    11.iterator

---
+++ 11.iterator/iterator.lua
@@ -0,0 +1,6 @@
+array = {"Lua", "Tutorial"}
+
+for key,value in ipairs(array) 
+do
+   print(key, value)
+end
---
+++ 11.iterator/iterator_function.lua
@@ -0,0 +1,12 @@
+function square(iteratorMaxCount,currentNumber)
+   if currentNumber<iteratorMaxCount
+   then
+      currentNumber = currentNumber+1
+   return currentNumber, currentNumber*currentNumber
+   end
+end
+
+for i,n in square,3,0
+do
+   print(i,n)
+end
