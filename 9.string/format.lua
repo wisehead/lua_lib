8657fadadb1f733d7ed727e255460c50972157f5
wisehead <alex.chenhui@gmail.com>
2019-03-25 14:45:57 +0800

    9.string part1

---
+++ 9.string/format.lua
@@ -0,0 +1,11 @@
+string1 = "Lua"
+string2 = "Tutorial"
+number1 = 10
+number2 = 20
+-- 基本字符串格式化
+print(string.format("基本格式化 %s %s",string1,string2))
+-- 日期格式化
+date = 2; month = 1; year = 2014
+print(string.format("日期格式化 %02d/%02d/%03d", date, month, year))
+-- 十进制格式化
+print(string.format("%.4f",1/3))
---
+++ 9.string/format2.lua
@@ -0,0 +1,14 @@
+string.format("%c", 83)    		        --输出S
+string.format("%+d", 17.0)              --输出+17
+string.format("%05d", 17)               --输出00017
+string.format("%o", 17)                 --输出21
+--string.format("%u", 3.14)               --输出3
+string.format("%x", 13)                 --输出d
+string.format("%X", 13)                 --输出D
+string.format("%e", 1000)               --输出1.000000e+03
+string.format("%E", 1000)               --输出1.000000E+03
+string.format("%6.3f", 13)              --输出13.000
+string.format("%q", "One\nTwo")         --输出"One\ 　　Two"
+string.format("%s", "monkey")           --输出monkey
+string.format("%10s", "monkey")         --输出    monkey
+string.format("%5.3s", "monkey")        --输出  mon
